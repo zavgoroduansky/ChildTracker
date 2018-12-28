95c825a341f10fa2d47ee3655b2268541d8ea296
Oleg Zavgorodianskyi <o.zavgorodianskyi@tstechpro.com>
2018-12-28 15:48:09 +0200

    fix

--- ChildTracker/Presenters/AddNewAction/AddNewActionViewControllerPresenter.swift
+++ ChildTracker/Presenters/AddNewAction/AddNewActionViewControllerPresenter.swift
@@ -23,7 +23,7 @@ class AddNewActionViewControllerPresenter: NSObject {
         return false
     }
     
-    public var newAction: NewAction = NewAction()
+    public var newAction: NewAction = NewAction()   // need to think about it and totaly removed
     
     private var showValuePicker = false
     private var showDatePicker = false
@@ -136,6 +136,24 @@ extension AddNewActionViewControllerPresenter: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         switch indexPath.section {
+        case 1:
+            if indexPath.row == 0 {
+                let cell = tableView.dequeueReusableCell(withIdentifier: BasicOpenableTableViewCell.identifier, for: indexPath) as! BasicOpenableTableViewCell
+                
+                cell.titleLabel.text = "Date:"
+                cell.valueLabel.text = newAction.date.datePickerFormatString
+                
+                return cell
+            } else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: DatePickerTableViewCell.identifier, for: indexPath) as! DatePickerTableViewCell
+                
+                cell.valueChangedHandler = { [unowned self = self] sender in
+                    self.newAction.date = sender.date
+                    tableView.reloadRows(at: [IndexPath(row: 0, section: indexPath.section)], with: .automatic)
+                }
+                
+                return cell
+            }
         case 2:
             if indexPath.row == 0 {
                 let cell = tableView.dequeueReusableCell(withIdentifier: BasicOpenableTableViewCell.identifier, for: indexPath) as! BasicOpenableTableViewCell
--- ChildTracker/Presenters/AddNewAction/Types/AddNewDeficationViewControllerPresenter.swift
+++ ChildTracker/Presenters/AddNewAction/Types/AddNewDeficationViewControllerPresenter.swift
@@ -25,33 +25,3 @@ class AddNewDeficationViewControllerPresenter: AddNewActionViewControllerPresent
         }
     }
 }
-
-// MARK: UITableViewDataSource
-extension AddNewDeficationViewControllerPresenter {
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        switch indexPath.section {
-        case 1:
-            if indexPath.row == 0 {
-                let cell = tableView.dequeueReusableCell(withIdentifier: BasicOpenableTableViewCell.identifier, for: indexPath) as! BasicOpenableTableViewCell
-                
-                cell.titleLabel.text = "Date:"
-                cell.valueLabel.text = newAction.date.datePickerFormatString
-                
-                return cell
-            } else {
-                let cell = tableView.dequeueReusableCell(withIdentifier: DatePickerTableViewCell.identifier, for: indexPath) as! DatePickerTableViewCell
-                
-                cell.valueChangedHandler = { [unowned self = self] sender in
-                    self.newAction.date = sender.date
-                    tableView.reloadRows(at: [IndexPath(row: 0, section: indexPath.section)], with: .automatic)
-                }
-                
-                return cell
-            }
-        default:
-            return super.tableView(tableView, cellForRowAt: indexPath)
-        }
-    }
-}
